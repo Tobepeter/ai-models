3afd459aa11b5ed4c97a67c178ced4d6821e73c9
tobe <583590744@qq.com>
2025-07-28 00:32:11 +0800

    code adjust

--- src/pages/test/test-mgr.ts
+++ src/pages/test/test-mgr.ts
@@ -1,9 +1,8 @@
 import { isDev } from '@/utils/env'
-import { TestCache, TestComponent, TestComponentMap } from './test-types'
 
 let testMgr = {
-	testComponentsMap: {} as TestComponentMap,
-	testCache: {} as TestCache,
+	compMap: {},
+	cache: {},
 	init: () => {},
 	load: async (path: string) => null as any,
 	getKeys: () => [] as string[],
@@ -11,53 +10,35 @@ let testMgr = {
 
 if (isDev) {
 	class TestMgr {
-		testComponentsMap: TestComponentMap
-		testCache: TestCache
-
-		constructor() {
-			this.testComponentsMap = {}
-			this.testCache = {}
-		}
+		compMap = {}
+		cache = {}
 
 		init() {
-			// 使用 glob 获取所有 test- 组件
 			const files = import.meta.glob('./components/test-*.tsx')
-
-			this.testComponentsMap = {}
-			this.testCache = {}
+			this.compMap = {}
+			this.cache = {}
 
 			for (const path of Object.keys(files)) {
-				const pathParts = path.split('/')
-				const fileName = pathParts[pathParts.length - 1]
-				// 移除 test- 前缀和 .tsx 后缀
-				const name = fileName.replace('test-', '').replace('.tsx', '')
-
-				this.testComponentsMap[name] = {
+				// 移除前缀和文件后缀
+				let name = path.split('/').pop() || ''
+				name = name.replace(/^test-|\.tsx$/g, '')
+				this.compMap[name] = {
 					path,
 					name,
-					load: async () => {
-						const mod = (await files[path]()) as any
-						// 使用默认导出
-						return mod.default
-					},
+					load: async () => ((await files[path]()) as any).default,
 				}
 			}
 		}
 
 		async load(name: string) {
-			const item = this.testComponentsMap[name]
-
+			const item = this.compMap[name]
 			if (!item) return null
-
-			if (this.testCache[name]) return this.testCache[name]
-
-			const result = await item.load()
-			this.testCache[name] = result
-			return result
+			if (this.cache[name]) return this.cache[name]
+			return (this.cache[name] = await item.load())
 		}
 
 		getKeys() {
-			return Object.keys(this.testComponentsMap)
+			return Object.keys(this.compMap)
 		}
 	}
 
